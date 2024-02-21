848a20bc36f0cc31ab01bdc72e0fe8e77605ef67
Nathan Whitney <86084524+the29ster@users.noreply.github.com>
2024-02-21 12:50:59 -0800

    Create models.py

---
+++ api/animalAdopter/database/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class AnimalModel(models.Model):
+    age = models.CharField(max_length = 280)
+    gender = models.CharField(max_length = 280)
+    price = models.CharField(max_length = 280)
+    type = models.CharField(max_length = 280)
