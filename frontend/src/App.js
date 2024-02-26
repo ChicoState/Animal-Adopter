93b2e4819ecaa03e6e880dfd62c0ef8a75293e5a
Ivan Transito <itransito@mail.csuchico.edu>
2024-02-26 12:06:37 -0800

    added a test image look

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,12 +13,25 @@ function App() {
     });
   }, []);
   return (
+  <body>
     <div className="App">
       <header className="App-header">
+      <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
+      <div class="kitty">
+        <img src="https://www.daysoftheyear.com/cdn-cgi/image/dpr=1%2Cf=auto%2Cfit=cover%2Cheight=675%2Cq=85%2Cwidth=1200/wp-content/uploads/international-black-cat-awareness-month-e1575287719521.jpg" alt="alternatetext"></img>
+        <div class="desc">
+          <p>Name: Marlo</p>
+          <p>Location: Chico</p>
+          <p>Breed: bombay cat</p>
+          <p>Sex: Male</p>
+          <p>Cost: Free</p>
+        </div>
+      </div>
       <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
 
       </header>
     </div>
+  </body>
   );
 }
 
