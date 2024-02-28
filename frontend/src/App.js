75631a2a809fbc0dd6a4acf4e364bbe56e2acb91
a13n20 <nhoffmann3@csuchico.edu>
2024-02-28 11:55:59 -0800

    Test for displaying data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,37 +2,51 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Adopt from "./pages/adopt.js";
 import Rehome from "./pages/rehome.js";
+import axios from  'axios'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [currentDate, setCurrentDate] = useState(0);
-  useEffect(() => {
-  fetch(' http://127.0.0.1:8000/').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-      setCurrentDate(data.date)
-    });
-  }, []);
-  return (
-  <body>
-    <div className="App">
-      <header className="App-header">
-      <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
-      <div class="kitty">
-        <img src="https://www.daysoftheyear.com/cdn-cgi/image/dpr=1%2Cf=auto%2Cfit=cover%2Cheight=675%2Cq=85%2Cwidth=1200/wp-content/uploads/international-black-cat-awareness-month-e1575287719521.jpg" alt="alternatetext"></img>
-        <div class="desc">
-          <p>Name: Marlo</p>
-          <p>Location: Chico</p>
-          <p>Breed: bombay cat</p>
-          <p>Sex: Male</p>
-          <p>Cost: Free</p>
-        </div>
-      </div>
-      <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
+	const [pet, setPet] = useState([]);
 
-      </header>
-    </div>
-  </body>
-  );
+	useEffect(() => { 
+		axios.get('http://localhost:3000/api/animalAdopter/models').then(res => {
+			console.log(res)
+			setPet(res.data.pet);
+		});
+	}, []);
+
+	var petDetails = "";
+	petDetails = pet.map( (item, index) => {
+		return (
+			<tr key={index}>
+			<td>{item.name}</td>
+			<td>{item.loc}</td>
+			<td>{item.breed}</td>
+			<td>{item.sex}</td>
+			<td>{item.cost}</td>
+			<td>{item.contact}</td>
+			</tr>
+		)
+	});
+	return (
+		<body>
+		<div className="App">
+		<header className="App-header">	
+		<div class="kitty">
+		<img src="https://www.daysoftheyear.com/cdn-cgi/image/dpr=1%2Cf=auto%2Cfit=cover%2Cheight=675%2Cq=85%2Cwidth=1200/wp-content/uploads/international-black-cat-awareness-month-e1575287719521.jpg" alt="alternatetext"></img>
+		<div class="desc">
+		<p>Name: {petDetails.item.name}</p>
+		<p>Location: {petDetails.item.loc}</p>
+		<p>Breed: {petDetails.item.breed}</p>
+		<p>Sex: {petDetails.item.sex}</p>
+		<p>Cost: {petDetails.item.cost}</p>
+		<p>Contact: {petDetails.item.contact}</p>
+		</div>
+		</div>
+
+		</header>
+		</div>
+		</body>
+	);
 }
 
 export default App;
