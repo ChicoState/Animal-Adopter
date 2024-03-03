a909a83ce93ed0ea98af807f178b36a2b0b59a34
a13n20 <nhoffmann3@csuchico.edu>
2024-03-03 14:08:32 -0800

    Network error occuring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,23 +4,59 @@ import Adopt from "./pages/adopt.js";
 import Rehome from "./pages/rehome.js";
 import axios from 'axios';
 
-function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [currentDate, setCurrentDate] = useState(0);
-  useEffect(() => {
-  fetch(' http://127.0.0.1:8000/').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-      setCurrentDate(data.date)
-    });
-  }, []);
-  return (
-    <div className="App">
-      <header className="App-header">
-      <p>The date is {currentDate} and the time is {currentTime}.</p> <br/>
+function App() { 
+	const [pet, setPet] = useState([]);
 
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		axios.get('http://localhosts:3000/api/animalAdopter/models').then(res => {
+			console.log(res)
+			setPet(res.data.pet);
+		});
+	}, []);
+
+	var petDetails = "";
+	petDetails = pet.map( (item, index) => {
+		return (
+			<tr key={index}>
+			<td>{item.age}</td>
+			<td>{item.gender}</td>
+			<td>{item.price}</td>
+			<td>{item.type}</td>
+			<td>{item.loc}</td>
+			<td>{item.image}</td>	
+			</tr>
+		)
+	});
+	return (
+		<div className="container">
+		<div className="row">
+		<div className="col-md-12">
+		<div className="card">
+		<div className="card-header">
+		<h4>Pet List</h4>
+		</div>
+		<div className="card-body">
+		<table className="table table-striped">
+		<thead>
+		<tr>
+		<th>Age</th>
+		<th>Gender</th>
+		<th>Price</th>
+		<th>Type</th>
+		<th>Location</th>
+		<th>Image</th>
+		</tr>
+		</thead>
+		<tbody>
+		{petDetails}
+		</tbody>
+		</table>
+		</div>
+		</div>
+		</div>
+		</div>
+		</div>
+	);
 }
 
 export default App;
