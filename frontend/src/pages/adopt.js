4e6a08a710c5bf46e6a2243836269c4bca261cb3
tnash321 <tnash321@gmail.com>
2024-03-11 12:11:16 -0700

    Created userLogin branch

--- frontend/src/pages/adopt.js
+++ frontend/src/pages/adopt.js
@@ -3,7 +3,7 @@ import React from "react";
 const AdoptionFormPage = () => {
   return (
     <div style={{ padding: '45px', textAlign: 'center' }}>
-      <h1 style={{ marginBottom: '20px' }}>Apply to Adopt a Pet</h1>
+      <h1 style={{ marginBottom: '20px' }}>Apply to Adopt a Pet </h1>
       <div style={{ maxWidth: '600px', margin: '0 auto' }}>
       </div>
     </div>
