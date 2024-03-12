cb5fd4a551be7af1a9565fb48975998084129d3d
a13n20 <143044296+a13n20@users.noreply.github.com>
2024-03-11 18:34:08 -0700

    Navbar adjusted with logo

--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { NavLink as Link } from "react-router-dom";
 export const Nav = styled.nav`
   position: fixed;
   background: #00524c;
-  height: 65px;
+  height: 85px;
   display: flex;
   justify-content: space-between;
   padding: 0 20px;
@@ -17,10 +17,17 @@ export const Nav = styled.nav`
 
 export const WebsiteName = styled.h1`
   color: #dfeeeb; /* Change the color to your preference */
-  font-size: 24px; /* Adjust the font size to your preference */
-  margin-right: 20px; /* Add some right margin for spacing */
+  font-size: 30px; /* Adjust the font size to your preference */
+  margin-top: 15px;
+  margin-left: 10px;
   font-family: 'SiteFont';
-  font-weight: normal;
+  font-weight: bold;
+`;
+
+export const LogoImg = styled.img`
+  height: 60px;
+  width: auto;
+  margin-right: 10px;
 `;
 
 export const NavLink = styled(Link)`
@@ -33,8 +40,14 @@ export const NavLink = styled(Link)`
   cursor: pointer;
   font-family: 'SiteFont';
   font-weight: normal;
+  padding: 5px 10px;
+  margin: -3px;
+  background: #529873;
+  border: 3px solid #529873;
+  border-bottom: none;
   &.active {
-    color: #6dcb99;
+    color: #529873;
+    background: #dfeeeb;
   }
 `;
 
@@ -55,7 +68,7 @@ export const Bars = styled(FaBars)`
 export const NavMenu = styled.div`
   display: flex;
   align-items: center;
-  margin-right: -24px;
+  margin-top: -14px;
 
   @media screen and (max-width: 768px) {
     display: none;
