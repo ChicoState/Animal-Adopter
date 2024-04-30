f2d94d517080a368d10cb4a1a5e62213cc4dbdc9
oliviajohnson91 <olivia@chicojohnsons.com>
2024-04-29 20:31:53 -0700

    Getting closer but still not working

--- api/animalAdopter/urls.py
+++ api/animalAdopter/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from .views import index
 from .views import get_animal_models
 from .views import create_animal_model, user_profile_view, user_animals_view
@@ -35,7 +34,6 @@ urlpatterns = [
     path('api/user_profile/', UserProfileCreateView.as_view(), name='user_profile_create'),
     path('api/user/profile/<str:username>/', user_profile_view, name='user-profile'),
     path('api/user/animals/<str:username>/', user_animals_view, name='user-animals'),
-    path('api/user_profile/<str:username>/', user_profile_view, name='user_profile_with_username'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -1,36 +1,112 @@
+// // import React, { useState, useEffect } from 'react';
+// // import { Nav, LogoImg, NavLink, Bars, NavMenu, WebsiteName } from "./Navbar/NavbarElements";
+// // import { FaUserCircle } from 'react-icons/fa';
+// // import Logo from "../logo/Logo.png";
+
+// // const Navbar = () => {
+// //   const [showDropdown, setShowDropdown] = useState(false);
+// //   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+// //   useEffect(() => {
+// //     const checkLogin = () => {
+// //       const token = localStorage.getItem('token');
+// //       setIsLoggedIn(!!token);
+// //     };
+
+// //     // Polling localStorage every 1000 milliseconds (1 second)
+// //     const intervalId = setInterval(checkLogin, 1000);
+
+// //     // Listen for changes in localStorage across tabs
+// //     const handleStorageChange = (event) => {
+// //       if (event.key === 'token') {
+// //         checkLogin();
+// //       }
+// //     };
+
+// //     window.addEventListener('storage', handleStorageChange);
+
+// //     // Cleanup the event listener and interval when the component unmounts
+// //     return () => {
+// //       window.removeEventListener('storage', handleStorageChange);
+// //       clearInterval(intervalId);
+// //     };
+// //   }, []);
+
+// //   const closeDropdown = () => {
+// //     setShowDropdown(false);
+// //   };
+
+// //   return (
+// //     <>
+// //       <Nav>
+// //         <div className="navbar-components-container" style={{ display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+// //           <LogoImg src={Logo} alt="Logo" />
+// //           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+// //             <WebsiteName>Animal Adopter</WebsiteName>
+// //             <Bars onClick={() => setShowDropdown(!showDropdown)} style={{ cursor: 'pointer' }} />
+// //             <div className="collapse navbar-collapse" id="navbarNav">
+// //               <NavMenu style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+// //                 <NavLink exact to="/" activeClassName="active" className="nav-link" onClick={closeDropdown}>Home</NavLink>
+// //                 <NavLink to="/adopt" activeClassName="active" className="nav-link" onClick={closeDropdown}>Adopt</NavLink>
+// //                 <NavLink to="/rehome" activeClassName="active" className="nav-link" onClick={closeDropdown}>Rehome</NavLink>
+// //                 <FaUserCircle size="4em" onClick={() => setShowDropdown(!showDropdown)} style={{ position: 'absolute', right: '1%', top: '15%', color: 'white', cursor: 'pointer', marginLeft: '20px' }} />
+// //               </NavMenu>
+// //               {showDropdown && (
+// //                 <div style={{ position: 'absolute', right: '1%', top: '100%', backgroundColor: 'white', padding: '10px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
+// //                   {isLoggedIn ? (
+// //                     <>
+// //                       <NavLink to="/profilePage" activeClassName="active" className="nav-link" onClick={closeDropdown}>Your Profile</NavLink>
+// //                       <NavLink to="/login" activeClassName="active" className="nav-link" onClick={closeDropdown}>Logout</NavLink>
+// //                     </>
+// //                   ) : (
+// //                     <NavLink to="/login" activeClassName="active" className="nav-link" onClick={closeDropdown}>Login</NavLink>
+// //                   )}
+// //                 </div>
+// //               )}
+// //             </div>
+// //           </div>
+// //         </div>
+// //       </Nav>
+// //     </>
+// //   );
+// // };
+
+// // export default Navbar;
+
+
 // import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
 // import { Nav, LogoImg, NavLink, Bars, NavMenu, WebsiteName } from "./Navbar/NavbarElements";
 // import { FaUserCircle } from 'react-icons/fa';
 // import Logo from "../logo/Logo.png";
 
 // const Navbar = () => {
+//   const username = localStorage.getItem('username') || 'default-username';
+//   const [user, setUser] = useState(null);
 //   const [showDropdown, setShowDropdown] = useState(false);
 //   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const [userImage, setUserImage] = useState(null);
 
 //   useEffect(() => {
-//     const checkLogin = () => {
-//       const token = localStorage.getItem('token');
-//       setIsLoggedIn(!!token);
+//     const authToken = localStorage.getItem('authToken');
+//     const config = {
+//       headers: { Authorization: `Token ${authToken}` }
 //     };
 
-//     // Polling localStorage every 1000 milliseconds (1 second)
-//     const intervalId = setInterval(checkLogin, 1000);
-
-//     // Listen for changes in localStorage across tabs
-//     const handleStorageChange = (event) => {
-//       if (event.key === 'token') {
-//         checkLogin();
-//       }
-//     };
-
-//     window.addEventListener('storage', handleStorageChange);
-
-//     // Cleanup the event listener and interval when the component unmounts
-//     return () => {
-//       window.removeEventListener('storage', handleStorageChange);
-//       clearInterval(intervalId);
-//     };
-//   }, []);
+//     axios.get(`http://127.0.0.1:8000/api/user/profile/${username}`, config)
+//       .then(response => {
+//         if (response.status === 200) {
+//           setIsLoggedIn = true;
+//           setUser(response.data);
+//         } else {
+//           setIsLoggedIn = false;
+//           console.error('Failed to fetch user profile:', response.status);
+//         }
+//       })
+//       .catch(error => {
+//         console.error('Error fetching user profile:', error.toString());
+//       });
+//   }, [username]);
 
 //   const closeDropdown = () => {
 //     setShowDropdown(false);
@@ -49,7 +125,11 @@
 //                 <NavLink exact to="/" activeClassName="active" className="nav-link" onClick={closeDropdown}>Home</NavLink>
 //                 <NavLink to="/adopt" activeClassName="active" className="nav-link" onClick={closeDropdown}>Adopt</NavLink>
 //                 <NavLink to="/rehome" activeClassName="active" className="nav-link" onClick={closeDropdown}>Rehome</NavLink>
-//                 <FaUserCircle size="4em" onClick={() => setShowDropdown(!showDropdown)} style={{ position: 'absolute', right: '1%', top: '15%', color: 'white', cursor: 'pointer', marginLeft: '20px' }} />
+//                 {userImage ?
+//                   <img src={userImage} alt="User Profile" style={{ width: '4em', height: '4em', borderRadius: '50%', position: 'absolute', right: '1%', top: '15%' }} onClick={() => setShowDropdown(!showDropdown)} />
+//                   :
+//                   <FaUserCircle size="4em" style={{ position: 'absolute', right: '1%', top: '15%', color: 'white', cursor: 'pointer', marginLeft: '20px' }} onClick={() => setShowDropdown(!showDropdown)} />
+//                 }
 //               </NavMenu>
 //               {showDropdown && (
 //                 <div style={{ position: 'absolute', right: '1%', top: '100%', backgroundColor: 'white', padding: '10px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)' }}>
@@ -73,7 +153,6 @@
 
 // export default Navbar;
 
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Nav, LogoImg, NavLink, Bars, NavMenu, WebsiteName } from "./Navbar/NavbarElements";
@@ -81,24 +160,34 @@ import { FaUserCircle } from 'react-icons/fa';
 import Logo from "../logo/Logo.png";
 
 const Navbar = () => {
+  const [user, setUser] = useState(null);
   const [showDropdown, setShowDropdown] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userImage, setUserImage] = useState(null);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    setIsLoggedIn(!!token);
+    const authToken = localStorage.getItem('authToken');
+    const config = {
+      headers: { Authorization: `Token ${authToken}` }
+    };
 
-    if (isLoggedIn) {
-      const fetchUserProfile = async () => {
-        const response = await axios.get('http://127.0.0.1:8000/api/user_profile/', {
-          headers: { 'Authorization': `Token ${token}` }
-        });
-        setUserImage(response.data.image);
-      };
-      fetchUserProfile();
-    }
-  }, [isLoggedIn]);
+    const username = localStorage.getItem('username') || 'default-username';
+    axios.get(`http://127.0.0.1:8000/api/user/profile/${username}`, config)
+      .then(response => {
+        if (response.status === 200) {
+          setIsLoggedIn(true);
+          setUser(response.data);
+          setUserImage(response.data.image);
+        } else {
+          setIsLoggedIn(false);
+          console.error('Failed to fetch user profile:', response.status);
+        }
+      })
+      .catch(error => {
+        setIsLoggedIn(false);
+        console.error('Error fetching user profile:', error.toString());
+      });
+  }, []);
 
   const closeDropdown = () => {
     setShowDropdown(false);
@@ -128,7 +217,11 @@ const Navbar = () => {
                   {isLoggedIn ? (
                     <>
                       <NavLink to="/profilePage" activeClassName="active" className="nav-link" onClick={closeDropdown}>Your Profile</NavLink>
-                      <NavLink to="/login" activeClassName="active" className="nav-link" onClick={closeDropdown}>Logout</NavLink>
+                      <NavLink to="/logout" activeClassName="active" className="nav-link" onClick={() => {
+                        localStorage.removeItem('authToken');
+                        setIsLoggedIn(false);
+                        closeDropdown();
+                      }}>Logout</NavLink>
                     </>
                   ) : (
                     <NavLink to="/login" activeClassName="active" className="nav-link" onClick={closeDropdown}>Login</NavLink>
