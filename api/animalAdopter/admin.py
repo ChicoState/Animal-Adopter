3b46c3eb1f0d4530a4718e5b0e1ce32cb29b0dbb
a13n20 <nhoffmann3@csuchico.edu>
2024-05-06 20:29:03 -0700

    id field shows up

--- api/animalAdopter/admin.py
+++ api/animalAdopter/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from . import models
-from .models import UserModel
-
-admin.site.register(models.AnimalModel)
+from .models import UserModel, AnimalModel
 
 @admin.register(UserModel)
 class UserModelAdmin(admin.ModelAdmin):
     list_display = [field.name for field in UserModel._meta.fields]  # This will display all fields
+
+@admin.register(AnimalModel)
+class AnimalModelAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in AnimalModel._meta.fields]
